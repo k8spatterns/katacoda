a196cf9db100d087d6a92b7fa97caf833a4eb784
Roland Huß <roland@ro14nd.de>
2019-11-29 12:34:35 +0100

    worked on

--- 21-Controller/install.sh
+++ 21-Controller/install.sh
@@ -3,3 +3,5 @@ bat_version=0.12.1
 curl -L -o /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v${bat_version}/bat_${bat_version}_amd64.deb
 dpkg -i /tmp/bat.deb
 rm /tmp/bat.deb
+
+launch-kubernetes.sh
